dc639de75dd83723f6d9c5c6edac3d51a2e8ae42
Jimmy <bruno_pier@hotmail.com>
2019-12-31 19:37:00 -0300

    Reordering code in App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1>Hi im React App</h1>
+            <h1>Hi im React App</h1>
         <Person name="Jimmy" age="30" />
         <Person name="Nahuel" age="32">My hobbies: Racing</Person>
         <Person name="Geronimo" age="29" />
