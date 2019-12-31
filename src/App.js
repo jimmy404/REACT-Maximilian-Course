77eee3b30a5ce9004c69131e0bb3a0bccddb0f75
Jimmy <bruno_pier@hotmail.com>
2019-12-31 19:35:43 -0300

    Adding the Children Property in App.js from Person.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi im React App</h1>
-      <Person name="Jimmy" age="30" />
-      <Person name="Nahuel" age="32" />
-      <Person name="Geronimo" age="29" />
+          <h1>Hi im React App</h1>
+        <Person name="Jimmy" age="30" />
+        <Person name="Nahuel" age="32">My hobbies: Racing</Person>
+        <Person name="Geronimo" age="29" />
       </div>
     );
   }
