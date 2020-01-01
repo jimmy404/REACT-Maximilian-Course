c6147bfe3a06c95a7d2b14f0b2a36fe18df501fd
Jimmy <bruno_pier@hotmail.com>
2020-01-01 19:59:37 -0300

    Adding this&this.state in App - Changes name&age

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Jimmy', age: 30},
+      { name: 'Nahuel', age: 32},
+      { name: 'Geronimo', age: 29}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
             <h1>Hi im React App</h1>
-        <Person name="Jimmy" age="30" />
-        <Person name="Nahuel" age="32">My hobbies: Racing</Person>
-        <Person name="Geronimo" age="29" />
+          <button>Swich name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
