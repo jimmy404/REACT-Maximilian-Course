4d418d58b4963ed54cca96710b5db3df4234f4c8
Jimmy <bruno_pier@hotmail.com>
2020-01-03 14:11:34 -0300

    Rendering Content Conditionally in App.js - Render if switch button

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       { name: 'Nahuel', age: 32 },
       { name: 'Geronimo', age: 29 }
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -34,12 +35,30 @@ class App extends Component {
     } )
   }
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
   render () {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Jimmy!!')}>Switch Name</button>
+        <button
+        style={style}
+        onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        { this.state.showPersons === true ?
+          <div>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age} />
@@ -51,6 +70,8 @@ class App extends Component {
         <Person
           name={this.state.persons[2].name}
           age={this.state.persons[2].age} />
+        </div> : null
+          }
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
