703861d1fdb564e786df178cddad265460434c5c
Jimmy <bruno_pier@hotmail.com>
2020-01-04 16:22:01 -0300

    Reordering code in Person.js - no changes

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,28 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 //import './Person.css';
 
-const person = ( props ) => {
+const SytledDiv = styled.div`
+    width: 60%;
+    margin: 16px auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
 
-    const style = {
-        '@media (min-width: 500px)':{
-            width: '450px'
-        }
-    }
-
-    const SytledDiv = styled.div`
-        width: 60%;
-        margin: 16px auto;
-        border: 1px solid #eee;
-        box-shadow: 0 2px 3px #ccc;
-        padding: 16px;
-        text-align: center;
-
-        @media (min-width: 500px) {
-            .Person {
-                width: 450px;
-                }
+    @media (min-width: 500px) {
+        .Person {
+            width: 450px;
             }
-        `;
+        }
+`;
+
+const person = ( props ) => {
 
     return (
         //<div className="Person" style={style}>
