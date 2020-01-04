3e29ed73e226a14ad1f8aeb1c3c3fad020b31dba
Jimmy <bruno_pier@hotmail.com>
2020-01-04 16:04:08 -0300

    Adding styled-components & Work with it in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium, {StyleRoot} from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -92,7 +91,6 @@ class App extends Component {
 
 
     return (
-      <StyleRoot>
         <div className="App">
             <h1>Hi, I'm a React App</h1>
             <p className={classes.join(' ')}>This is really working!</p>
@@ -101,10 +99,9 @@ class App extends Component {
             onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
         </div>
-      </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default Radium(App);
+export default App;
