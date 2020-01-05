1d5320f4d2210543524d5c38c0070cdd16d234e5
Jimmy <bruno_pier@hotmail.com>
2020-01-05 18:21:39 -0300

    Eliminating Error Boundary from Person.js

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,12 +2,8 @@ import React from 'react';
 import classes from './Person.css';
 
 
-const person = props => {
+const person = (props) => {
 
-    const rnd = Math.random();
-        if (rnd > 0.7){
-            throw new Error('Something went wrong');
-        }
 
     return (
         //<div className="Person" style={style}>
