3f59e983bfd20ea760fc2efb7c6cfc71fdf1ec28
Jimmy <bruno_pier@hotmail.com>
2020-01-08 19:12:36 -0300

    Working with 2st type of refs in App.js - testing

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -8,8 +8,14 @@ import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
 
+    constructor(props){
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
     componentDidMount() {
-        this.inputElement.focus();
+        //this.inputElementRef.focus();
+        this.inputElementRef.current.focus();
     }
 
     render() {
@@ -22,7 +28,8 @@ class Person extends Component {
                 <p key="i2">{this.props.children}</p>
                 <input
                 key="i3"
-                ref={(inputEl) => {this.inputElement = inputEl}}
+                //ref={(inputEl) => {this.inputElement = inputEl}}
+                ref={this.inputElementRef}
                 type="text"
                 onChange={this.props.changed}
                 value={this.props.name}
