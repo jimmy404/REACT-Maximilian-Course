dd1f8b63d2d1349a4772feaa8288ea15f2979538
Jimmy <bruno_pier@hotmail.com>
2020-01-08 17:17:29 -0300

    Adding import withClasses in Person.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Auxiliary';
 import classes from './Person.css';
+import withClass from '../hoc/withClass';
+
 
 class Person extends Component {
     render() {
@@ -23,4 +25,4 @@ class Person extends Component {
     }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
