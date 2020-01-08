6046f9a0944897ccaf9d284489347324bcafc882
Jimmy <bruno_pier@hotmail.com>
2020-01-08 18:02:19 -0300

    Setting up PropTypes - Person.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import Aux from '../../../hoc/Auxiliary';
 import classes from './Person.css';
@@ -24,5 +25,10 @@ class Person extends Component {
         );
     }
 }
-
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 export default withClass(Person, classes.Person);
