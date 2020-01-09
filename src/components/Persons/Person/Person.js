fd973a0cc3b2411e90cf62cc6f100ceee2186ae2
Jimmy <bruno_pier@hotmail.com>
2020-01-09 00:10:15 -0300

    Adding Context in Person.js - Authenticated code

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import Aux from '../../../hoc/Auxiliary';
 import classes from './Person.css';
 import withClass from '../../../hoc/withClass';
-
+import AuthContext from '../../../context/auth-context';
 
 class Person extends Component {
 
@@ -22,6 +22,9 @@ class Person extends Component {
         console.log('[Person.js] rendering...');
         return(
         <Aux>
+            <AuthContext.Consumer>
+                {(context) => context.authenticated ? <p>Authenticated</p> : <p>Please log in</p> }
+            </AuthContext.Consumer>
                 <p onClick={this.props.click}>
                 I'm {this.props.name} and I am {this.props.age} years old!
                 </p>
