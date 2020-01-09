fab33ff748e36b93a4e3ab6c03384e2ff8db6e6f
Jimmy <bruno_pier@hotmail.com>
2020-01-09 00:09:24 -0300

    Testing Context API - persons.js - NO CHANGES

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react';
 
 import Person from './Person/Person';
-
 class Persons extends PureComponent {
   // static getDerivedStateFromProps(props, state) {
   //   console.log('[Persons.js] getDerivedStateFromProps');
@@ -46,7 +45,7 @@ class Persons extends PureComponent {
 
     render() {
     console.log('[Persons.js] rendering...');
-    return this.props.persons.map((person, index) => {
+    return (this.props.persons.map((person, index) => {
     return (
             <Person
             click={() => this.props.clicked(index)}
@@ -56,8 +55,9 @@ class Persons extends PureComponent {
             changed={event => this.props.changed(event, person.id)}
             isAuth={this.props.isAuthenticated}
             />
+                );
+            })
         );
-        });
     }
 }
 
